f273a4f6a9b4fedef6e3ee072e428277556f541b
Utkarsh1520 <utkarshprakash15112000@gmail.com>
2024-04-05 18:27:18 +0530

    cssFixes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const items = document.getElementById('navOptions');
 
 function openMenu(){
     items.style.display='flex';
+    console.log('suc'/n);
 }
 
 menu.addEventListener('click', openMenu);
